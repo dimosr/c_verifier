7726f8602cc088919e05fef6491b4ea38c7a6090
Raptis Dimos <raptis.dimos@yahoo.gr>
2015-10-23 17:17:11 +0100

    transferred translation functionality into SsaRepresentation

--- tool/SsaRepresentation.java
+++ tool/SsaRepresentation.java
@@ -34,4 +34,70 @@ public class SsaRepresentation {
         return assertions;
     }
     
+    public String getText(FreshStructure fresh) {
+        StringBuilder smtFormula = new StringBuilder();
+            
+        /**  variables declarations  **/
+        Map<String, Integer> freshMapping = fresh.getAllVariablesMappings();
+        for(String variableName : freshMapping.keySet() ) {
+            for(int i = 0; i <= freshMapping.get(variableName); i++) {
+                int index = freshMapping.get(variableName);
+                smtFormula.append("int ").append(variableName).append(index).append("; \n");
+            }
+        }
+            
+        smtFormula.append("\n");
+            
+        /**   assignments - MUST use prefix operators **/
+        for(Assignment assignment : this.getAssignments()) {
+            smtFormula.append(" ( ").append(assignment.variableName).append(" == ");
+            smtFormula.append(assignment.expression.toString());
+            smtFormula.append(" ) ");
+            smtFormula.append(" && ");
+        }
+            
+        smtFormula.append("\n");
+            
+        /**  assertions **/
+        for(Assertion assertion : this.getAssertions()) {
+            smtFormula.append("assert ( ");
+            smtFormula.append(assertion.expression.toString());
+            smtFormula.append(" ); \n");
+        }
+        smtFormula.append(" ) ");
+            
+        return smtFormula.toString();
+    }
+    
+    public String translateToSmt(FreshStructure fresh){
+        StringBuilder smtFormula = new StringBuilder();
+        
+        /**  variables declarations  **/
+        Map<String, Integer> freshMapping = fresh.getAllVariablesMappings();
+        for(String variableName : freshMapping.keySet() ) {
+            for(int i = 0; i <= freshMapping.get(variableName); i++) {
+                int index = freshMapping.get(variableName);
+                smtFormula.append("(declare-fun ").append(variableName).append(index).append(" () (_ BitVec 32)) \n");
+            }
+        }
+        
+        smtFormula.append("\n");
+        
+        /**   assignments - MUST use prefix operators **/
+        
+        smtFormula.append("\n");
+            
+        /**  assertions **/
+        smtFormula.append(" !( ");
+        for(Assertion assertion : this.getAssertions()) {
+            smtFormula.append(" ( ");
+            smtFormula.append(assertion.expression.toString());
+            smtFormula.append(" ) ");
+            smtFormula.append(" && ");
+        }
+        smtFormula.append(" ) ");
+        
+        return smtFormula.toString();
+    }
+    
 }
