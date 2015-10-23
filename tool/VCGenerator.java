4658d8e6cf63b0c76ed9f37a8594e053f4fe195a
Raptis Dimos <raptis.dimos@yahoo.gr>
2015-10-23 18:09:09 +0100

    transferred Translation to SsaRepresentation + added String output of SSA in text for debugging

--- tool/VCGenerator.java
+++ tool/VCGenerator.java
@@ -1,4 +1,7 @@
 package tool;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
 import java.util.Map;
 import parser.SimpleCParser.ProcedureDeclContext;
 import util.assertions.Assertion;
@@ -16,53 +19,25 @@ public class VCGenerator {
 		// TODO: You will probably find it useful to add more fields and constructor arguments
 	}
 	
-	public StringBuilder generateVC() {
+	public StringBuilder generateVC() throws FileNotFoundException {
+                verifierVisitor.visitProcedureDecl(proc);
+                FreshStructure fresh = verifierVisitor.getFresh();
+                SsaRepresentation ssa = verifierVisitor.getSsa();
+            
+                try (PrintStream out = new PrintStream(new FileOutputStream("ssa_format.txt"))) {
+                    out.print(ssa.getText(fresh));
+                }
+            
 		StringBuilder result = new StringBuilder("(set-logic QF_BV)\n");
 		result.append("(set-option :produce-models true)\n");
 		result.append("(define-fun tobv32 ((p Bool)) (_ BitVec 32) (ite p (_ bv1 32) (_ bv0 32)))\n");
 		result.append("(define-fun tobool ((p (_ BitVec 32))) Bool (ite (= p (_ bv0 32)) false true))\n");
 		
-		// TODO: generate the meat of the VC
-                verifierVisitor.visitProcedureDecl(proc);
-                SsaRepresentation ssa = verifierVisitor.getSsa();
-                result.append(ssaToSmtTranslate(ssa));
+                //result.append(ssa.translateToSmt(fresh));
 		
 		result.append("\n(check-sat)\n");
                 System.out.println(result);
 		return result;
 	}
-        
-        private String ssaToSmtTranslate(SsaRepresentation ssa){
-            StringBuilder smtFormula = new StringBuilder();
-            
-            Map<String, Integer> freshMapping = verifierVisitor.getFresh().getAllVariablesMappings();
-            for(String variableName : freshMapping.keySet() ) {
-                for(int i = 0; i <= freshMapping.get(variableName); i++) {
-                    int index = freshMapping.get(variableName);
-                    smtFormula.append("(declare-fun ").append(variableName).append(index).append(" () (_ BitVec 32))");
-                }
-            }
-            
-            smtFormula.append("\n");
-            
-            for(Assignment assignment : ssa.getAssignments()) {
-                smtFormula.append(" ( ").append(assignment.variableName).append(" == ");
-                smtFormula.append(assignment.expression.toString());
-                smtFormula.append(" ) ");
-                smtFormula.append(" && ");
-            }
-            
-            smtFormula.append("\n");
-            
-            smtFormula.append(" !( ");
-            for(Assertion assertion : ssa.getAssertions()) {
-                smtFormula.append(" ( ");
-                smtFormula.append(assertion.expression.toString());
-                smtFormula.append(" ) ");
-            }
-            smtFormula.append(" ) ");
-            
-            return smtFormula.toString();
-        }
 
 }
