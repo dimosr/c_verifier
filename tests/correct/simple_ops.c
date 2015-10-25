81116c9f3a5e00e31966196e68697b6db651291f
Raptis Dimos <raptis.dimos@yahoo.gr>
2015-10-25 17:29:18 +0000

    added one more test

---
+++ tests/correct/simple_ops.c
@@ -0,0 +1,20 @@
+// RUN: %tool "%s" > "%t"
+// RUN: %diff %CORRECT "%t"
+
+int foo() {
+
+    int x;
+    int y;
+    x = 20;
+    y = (x - 5)*4;
+    assert (y - 10 ) == 50;
+    int z;
+    int k;
+    k = 2;
+    k = (2 + x)*k;
+    assert k == 44;
+    k = k - 4;
+    
+    assert (k == 40) && (y == 15) && (x < 21);
+    return 0;
+}
