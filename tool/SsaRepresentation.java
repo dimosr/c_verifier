944e65f731ecac5d14302ad56e87142371a27046
Raptis Dimos <raptis.dimos@yahoo.gr>
2015-10-31 16:21:09 +0000

    added pseudoSMT without boolean-integer conversions for debugging

--- tool/SsaRepresentation.java
+++ tool/SsaRepresentation.java
@@ -92,30 +92,6 @@ public class SsaRepresentation {
         return divChecks.toString();
     }
     
-    public String translateToPseudoSmt(FreshStructure fresh){
-        StringBuilder pseudoSMT = new StringBuilder();
-        
-        /**   assignments - MUST use prefix operators **/
-        for(Assignment assignment : this.getAssignments()) {
-            pseudoSMT.append("( ").append(assignment.variableName).append(" == ");
-            pseudoSMT.append(getExpressionSsa(assignment.expression)).append(") && \n");
-        }
-        pseudoSMT.append("\n");
-            
-        /**  assertions **/
-        pseudoSMT.append(" !( \n");
-        for(Assertion assertion : this.getAssertions()) {
-            pseudoSMT.append("\t ( ");
-            pseudoSMT.append("(tobool ").append(getExpressionSsa(assertion.expression)).append(" )");
-            pseudoSMT.append(" ) ");
-            // fix last &&
-            pseudoSMT.append(" && \n");
-        }
-        pseudoSMT.append(" ) ");
-        
-        return pseudoSMT.toString();
-    }
-    
     public String translateToSmtFormula(FreshStructure fresh) {
         StringBuilder smtFormula = new StringBuilder();
         
@@ -273,6 +249,115 @@ public class SsaRepresentation {
             smtFormula.append(getExpressionSMT(unaryExpr.expr));
             for(int i = 0; i < parenthesesSum; i++)
                 smtFormula.append(" )");
+        }
+        else if(expression.getType() == ExpressionType.VARIABLE_REFERENCE) {
+            VarRefExpression varRefExpr = (VarRefExpression) expression;
+            smtFormula.append(varRefExpr.variableName);
+        }
+        
+        return smtFormula.toString();
+    }
+    
+    //SMT without tobool,tobv32 conversions
+    public String translateToPseudoSmt(FreshStructure fresh){
+        StringBuilder smtFormula = new StringBuilder();
+        
+        /**  variables declarations  **/
+        Map<String, Integer> freshMapping = fresh.getAllVariablesMappings();
+        for(String variableName : freshMapping.keySet() ) {
+            for(int i = 0; i <= freshMapping.get(variableName); i++) {
+                smtFormula.append("(declare-fun ").append(variableName).append(i).append(" () (_ BitVec 32)) \n");
+            }
+        }
+        
+        smtFormula.append("\n");
+        
+        /**   assignments - MUST use prefix operators **/    
+        for(Assignment assignment : this.getAssignments()) {
+            smtFormula.append("(assert (= ")
+                      .append(assignment.variableName).append(" ");
+            smtFormula.append(getExpressionPseudoSMT(assignment.expression)).append("))\n");
+            smtFormula.append(addZeroDivChecks());
+        }
+
+        smtFormula.append("\n");
+            
+        /**  assertions - MUST use prefix operators**/
+        smtFormula.append("(assert (not (and \n");
+        for(Assertion assertion : this.getAssertions()) { 
+            smtFormula.append(getExpressionPseudoSMT(assertion.expression)).append("\n");
+            smtFormula.append(addZeroDivChecks());
+        }
+        smtFormula.append("\n) ) )");
+        
+        return smtFormula.toString();
+    }
+    
+    //SMT without tobool,tobv32 conversions
+    private String getExpressionPseudoSMT(Expression expression) {
+        StringBuilder smtFormula = new StringBuilder();
+        
+        if(expression.getType() == ExpressionType.BINARY){
+            BinaryExpression binExpr = (BinaryExpression) expression;
+            if(binExpr.operator.opType.isNumInputNumOutput()) {
+                smtFormula.append("(").append(binExpr.operator.opType.smtForm()).append(" ");
+                smtFormula.append(getExpressionPseudoSMT(binExpr.leftExpr)).append(" ");
+                smtFormula.append(getExpressionPseudoSMT(binExpr.rightExpr)).append(")");
+                if(binExpr.operator.opType == BinaryOperatorType.DIV)
+                    divOperands.add(binExpr.rightExpr);
+            }
+            else if(binExpr.operator.opType.isNumInputBoolOutput()){
+                smtFormula.append("(tobv32 ");
+                
+                if(binExpr.operator.opType == BinaryOperatorType.NOT_EQUALS) {      //specific case, not_equals requires 2 symbols in SMT
+                    smtFormula.append("(not (= ");
+                    smtFormula.append(getExpressionPseudoSMT(binExpr.leftExpr)).append(" ");
+                    smtFormula.append(getExpressionPseudoSMT(binExpr.rightExpr)).append(" ");
+                    smtFormula.append(") )");
+                }
+                else{
+                    smtFormula.append("(").append(binExpr.operator.opType.smtForm()).append(" ");
+                    smtFormula.append(getExpressionPseudoSMT(binExpr.leftExpr)).append(" ");
+                    smtFormula.append(getExpressionPseudoSMT(binExpr.rightExpr)).append(" ");
+                    smtFormula.append(")");
+                }
+                
+                smtFormula.append(")");
+            }
+            else if(binExpr.operator.opType.isBoolInputBoolOutput()){
+                smtFormula.append("(").append(binExpr.operator.opType.smtForm()).append(" ");         
+                smtFormula.append(getExpressionPseudoSMT(binExpr.leftExpr));           
+                smtFormula.append(getExpressionPseudoSMT(binExpr.rightExpr)).append(") ");
+            }
+            
+        }
+        else if(expression.getType() == ExpressionType.CONSTANT) {
+            ConstantExpression constExpr = (ConstantExpression) expression;
+            smtFormula.append("(_ bv").append(constExpr.intValue).append(" 32)");
+        }
+        else if(expression.getType() == ExpressionType.OLD) {
+            // TO DO
+        }
+        else if(expression.getType() == ExpressionType.PARENTHESIS){
+            ParenthesisExpression parenExpr = (ParenthesisExpression) expression;
+            smtFormula.append(getExpressionPseudoSMT(parenExpr.expr));
+        }
+        else if(expression.getType() == ExpressionType.TERNARY) {
+            TernaryExpression ternExpr = (TernaryExpression) expression;
+            smtFormula.append("(ite ").append(getExpressionPseudoSMT(ternExpr.condExpr)).append(getExpressionSMT(ternExpr.ifExpr)).append(" ").append(getExpressionSMT(ternExpr.elseExpr)).append(")");
+        }
+        else if(expression.getType() == ExpressionType.UNARY) {
+            UnaryExpression unaryExpr = (UnaryExpression) expression;
+            
+            for(UnaryOperator operator : unaryExpr.operators) {
+                if(operator.opType == UnaryOperatorType.MINUS){     // unary + can be omitted (optimization)
+                    smtFormula.append("(").append(operator.opType.smtForm());
+                }
+                else if( (operator.opType == UnaryOperatorType.NOT) || (operator.opType == UnaryOperatorType.BNOT) ){
+                    smtFormula.append("(tobv32 (").append(operator.opType.smtForm()).append(")");
+                }
+            }
+            smtFormula.append(getExpressionPseudoSMT(unaryExpr.expr));
         }
         else if(expression.getType() == ExpressionType.VARIABLE_REFERENCE) {
             VarRefExpression varRefExpr = (VarRefExpression) expression;
