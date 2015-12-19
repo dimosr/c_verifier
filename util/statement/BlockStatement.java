dfffc3f53f361de7da50e71c6e7d818011eca61e
Raptis Dimos <raptis.dimos@yahoo.gr>
2015-12-19 19:18:41 +0000

    Added LICENSE

--- util/statement/BlockStatement.java
+++ util/statement/BlockStatement.java
@@ -1,3 +1,11 @@
+/**
+ *  Copyright 2016 by Dimos Raptis <raptis.dimos@yahoo.gr>
+ *  Licensed under GNU General Public License 2.0 or later. 
+ *  Some rights reserved. See LICENSE.
+ *
+ *  @license GPL-2.0 <http://spdx.org/licenses/GPL-2.0.html>
+ */
+
 package util.statement;
 
 import java.util.ArrayList;
